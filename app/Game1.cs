67178701c2e2311436cfa492bf4a7b54a442065a
Alexander Lemkin <alexanderlemkin@gmail.com>
2018-06-22 23:38:59 -0400

    Added sprites, character, movement

--- app/Game1.cs
+++ app/Game1.cs
@@ -4,83 +4,99 @@ using Microsoft.Xna.Framework.Input;
 
 namespace app.Desktop
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
-    public class Game1 : Game {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+  public class Game1 : Game {
+    GraphicsDeviceManager graphics;
+    InputHelper inputHelper;
 
-		Texture2D hillBackground;
+    SpriteBatch spriteBatch;
 
-		SpriteFont title;
+    SpriteFont title;
 
-        public Game1() {
-            graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-        }
+    Texture2D hillBackground;
+    Texture2D bill;
+		Vector2 billPosition;
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
-        protected override void Initialize() {
-            // TODO: Add your initialization logic here
-
-            base.Initialize();
-        }
-
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
-        protected override void LoadContent() {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-			title = Content.Load<SpriteFont>("Font");
-			hillBackground = Content.Load<Texture2D>("hillBackground");
-            // TODO: use this.Content to load your game content here
-        }
-
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
-        protected override void UnloadContent() {
-            // TODO: Unload any non ContentManager content here
-        }
-
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Update(GameTime gameTime) {
-			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) {
-				Exit();            
-            }
-
-            // TODO: Add your update logic here
-
-            base.Update(gameTime);
-        }
-
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Draw(GameTime gameTime) {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
-			spriteBatch.Begin();
-
-			spriteBatch.Draw(hillBackground, new Rectangle(0, 0, 800, 480), Color.White);
-            spriteBatch.DrawString(title, "Starting Platformer", new Vector2(0, 0), Color.White);
-            spriteBatch.End();
-            base.Draw(gameTime);
-        }
+    public Game1() {
+      graphics = new GraphicsDeviceManager(this);
+      Content.RootDirectory = "Content";
     }
+
+    /// <summary>
+    /// Allows the game to perform any initialization it needs to before starting to run.
+    /// This is where it can query for any required services and load any non-graphic
+    /// related content.  Calling base.Initialize will enumerate through any components
+    /// and initialize them as well.
+    /// </summary>
+    protected override void Initialize() {
+        base.Initialize();
+  		inputHelper = new InputHelper();
+			billPosition = new Vector2(10, 10);
+    }
+
+    /// <summary>
+    /// LoadContent will be called once per game and is the place to load
+    /// all of your content.
+    /// </summary>
+    protected override void LoadContent() {
+      // Create a new SpriteBatch, which can be used to draw textures.
+      spriteBatch = new SpriteBatch(GraphicsDevice);
+    	title = Content.Load<SpriteFont>("Font");
+    	bill = Content.Load<Texture2D>("shot/1");
+    	hillBackground = Content.Load<Texture2D>("hillBackground");
+      // TODO: use this.Content to load your game content here
+    }
+
+    /// <summary>
+    /// UnloadContent will be called once per game and is the place to unload
+    /// game-specific content.
+    /// </summary>
+    protected override void UnloadContent() {
+        // TODO: Unload any non ContentManager content here
+    }
+
+    /// <summary>
+    /// Allows the game to run logic such as updating the world,
+    /// checking for collisions, gathering input, and playing audio.
+    /// </summary>
+    /// <param name="gameTime">Provides a snapshot of timing values.</param>
+    protected override void Update(GameTime gameTime) {
+      inputHelper.Update();
+
+      if (inputHelper.IsNewKeyPress(Keys.Escape)) { Exit(); }
+
+			if (inputHelper.IsKeyDown(Keys.Right)) {
+				billPosition.X += 5;
+      }
+
+			if (inputHelper.IsKeyDown(Keys.Left)) {
+        billPosition.X -= 5;
+      }
+
+			if (inputHelper.IsKeyDown(Keys.Down)) {
+        billPosition.Y += 5;
+      }
+
+			if (inputHelper.IsKeyDown(Keys.Up)) {
+        billPosition.Y -= 5;
+      }
+
+      base.Update(gameTime);
+    }
+
+    /// <summary>
+    /// This is called when the game should draw itself.
+    /// </summary>
+    /// <param name="gameTime">Provides a snapshot of timing values.</param>
+    protected override void Draw(GameTime gameTime) {
+      GraphicsDevice.Clear(Color.CornflowerBlue);
+      string currentGameTimeInSeconds = gameTime.TotalGameTime.Seconds.ToString();
+      // TODO: Add your drawing code here
+      spriteBatch.Begin();
+
+      spriteBatch.Draw(hillBackground, new Rectangle(0, 0, 800, 480), Color.White);
+			spriteBatch.Draw(bill, billPosition, Color.White);
+      spriteBatch.DrawString(title, "Elapsed Time: " + currentGameTimeInSeconds, new Vector2(20, 5), Color.White);
+      spriteBatch.End();
+    }
+  }
 }
---
+++ app/InputHelper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace app.Desktop {
+	public enum MouseButtons { LeftButton, RightButton }
+
+    public class InputHelper {
+
+		public KeyboardState currentKeyboardState = new KeyboardState();
+		public MouseState currentMouseState = new MouseState();
+		public KeyboardState lastKeyboardState = new KeyboardState();
+		public MouseState lastMouseState = new MouseState();
+		public Vector2 cursorPos = new Vector2(0, 0);
+
+		public InputHelper() { }
+
+		public void Update()
+		{
+			lastKeyboardState = currentKeyboardState;
+			lastMouseState = currentMouseState;
+
+			currentKeyboardState = Keyboard.GetState();
+			currentMouseState = Mouse.GetState();
+
+			//track cursor position
+			cursorPos.X = currentMouseState.X;
+			cursorPos.Y = currentMouseState.Y;
+		}
+
+		//check for keyboard key press, hold, and release
+		public bool IsNewKeyPress(Keys key)
+		{
+			return (currentKeyboardState.IsKeyDown(key) &&
+				lastKeyboardState.IsKeyUp(key));
+		}
+
+		public bool IsKeyDown(Keys key)
+		{
+			return (currentKeyboardState.IsKeyDown(key));
+		}
+
+		public bool IsNewKeyRelease(Keys key)
+		{
+			return (lastKeyboardState.IsKeyDown(key) &&
+				currentKeyboardState.IsKeyUp(key));
+		}
+
+		public bool IsNewMouseButtonPress(MouseButtons button)
+		{
+			//check to see the mouse button was pressed
+			if (button == MouseButtons.LeftButton)
+			{
+				return (currentMouseState.LeftButton == ButtonState.Pressed &&
+					lastMouseState.LeftButton == ButtonState.Released);
+			}
+			else if (button == MouseButtons.RightButton)
+			{
+				return (currentMouseState.RightButton == ButtonState.Pressed &&
+					lastMouseState.RightButton == ButtonState.Released);
+			}
+			else { return false; }
+		}
+
+		public bool IsNewMouseButtonRelease(MouseButtons button)
+		{   //check to see the mouse button was released
+			if (button == MouseButtons.LeftButton)
+			{
+				return (lastMouseState.LeftButton == ButtonState.Pressed &&
+					currentMouseState.LeftButton == ButtonState.Released);
+			}
+			else if (button == MouseButtons.RightButton)
+			{
+				return (lastMouseState.RightButton == ButtonState.Pressed &&
+					currentMouseState.RightButton == ButtonState.Released);
+			}
+			else { return false; }
+		}
+
+		public Boolean IsMouseButtonDown(MouseButtons button)
+		{
+			//check to see if the mouse button is being held down
+			if (button == MouseButtons.LeftButton)
+			{ return (currentMouseState.LeftButton == ButtonState.Pressed); }
+			else if (button == MouseButtons.RightButton)
+			{ return (currentMouseState.RightButton == ButtonState.Pressed); }
+			else { return false; }
+		}
+    }
+}
